b803087a47669927c2662ddb25cc204ec870649d
srbans007 <sr_bans007@hotmail.com>
2018-12-06 21:14:03 -0300

    se agrega la funcion para guardar en img-uploads

--- index.php
+++ index.php
@@ -9,41 +9,62 @@ include 'variables.php';
         <meta charset="utf-8">
         <!-- include libraries(jQuery, bootstrap, summernote) -->
         <link href="css/bootstrap.css" rel="stylesheet">
-        <script src="js/jquery.js"></script> 
+        <script src="js/jquery.js"></script>
         <script src="js/bootstrap.js"></script>
         <link href="dist/summernote.css" rel="stylesheet">
         <script src="dist/summernote.js"></script>
-        <script type="text/javascript" src="dist/lang/summernote-es-ES.js"></script>
-        <link rel="stylesheet" href="css/example.css">
-        <script type="text/javascript">
-            $(document).ready(function () {
-                $('.summernote').summernote({
-                    height: 200,
-                    tabsize: 2,
-                    lang: 'es-ES'
-                });
+        <script>$(document).ready(function() {
+        $("#summernote").summernote({
+          placeholder: '...',
+                height: 300,
+                 callbacks: {
+                onImageUpload : function(files, editor, welEditable) {
+
+                     for(var i = files.length - 1; i >= 0; i--) {
+                             sendFile(files[i], this);
+                    }
+                }
+            }
             });
+        });
+        function sendFile(file, el) {
+        var form_data = new FormData();
+        form_data.append('file', file);
+        $.ajax({
+            data: form_data,
+            type: "POST",
+            url: 'editor-upload.php',
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function(url) {
+                $(el).summernote('editor.insertImage', url);
+            }
+        });
+        }
         </script>
         <!-- include quicksearch -->
         <script src="js/jquery.quicksearch.js"></script>
     </head>
+    <body>
     <div class="container-fluid">
         <h4>Bienvenido a REC007.</h4>
         <!-- Editor de texto enriquecido -->
         <form action="crearArchivo.php" method="post">
             <textarea class="summernote" id="summernote" name="editordata"></textarea>
             <input class="form-control" type="text" name="nArch" placeholder="Nombre de archivo: (nombre.txt nombre.html, nombre.htm etc...)"><br>
-            <input type="submit" class='btn btn-success' value="Crear Archivo">	
+            <input type="submit" class='btn btn-success' value="Crear Archivo">
         </form><br>
+
         <!-- Barra de busqueda -->
         <input class="form-control" name="query" id="txt_query" placeholder="Busque aquí su archivo" type="text"><p>
 
         <!-- Listado de busqueda -->
         <form action="pag4.php" method="post">
-        
+
         <table class="table" id="table">
             <thead>
-                <tr>                    
+                <tr>
                     <th>Nombre</th>
                     <th>Acción</th>
                 </tr>
@@ -55,4 +76,5 @@ include 'variables.php';
         </table>
         </form>
     </div>
+  </body>
 </html>
